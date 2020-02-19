3c50e719cf2583a4e46cb4ae117afcd8477a7197
Otavio Gaiao <home@Otavios-Macbook-Pro.local>
2020-02-19 20:19:12 -0300

    initial commit

---
+++ App.js
@@ -0,0 +1,56 @@
+import React, {useState} from 'react';
+import {View, Button, Platform, StyleSheet} from 'react-native';
+import DateTimePicker from '@react-native-community/datetimepicker';
+
+
+const App = () => {
+  const [date, setDate] = useState(new Date(1598051730000));
+  const [mode, setMode] = useState('date');
+  const [show, setShow] = useState(false);
+
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate || date;
+
+    setDate(currentDate);
+    setShow(Platform.OS === 'ios' ? true : false);
+  };
+
+  const showMode = currentMode => {
+    setShow(true);
+    setMode(currentMode);
+  };
+
+  const showDatepicker = () => {
+    showMode('date');
+  };
+
+  return (
+    <View style={styles.container}>
+      <View>
+        <Button onPress={showDatepicker} title="Show date picker!" />
+      </View>
+      {show && (
+        <DateTimePicker
+          testID="dateTimePicker"
+          timeZoneOffsetInMinutes={0}
+          value={date}
+          mode={mode}
+          is24Hour={true}
+          display="spinner"
+          onChange={onChange}
+        />
+      )}
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
